29a1e3b63f365eb33173d754fd7dc2ecf0732753
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2025-04-08 17:02:52 +0000

    Added test Schema

--- src/crc1415testsample/schema_packages/__init__.py
+++ src/crc1415testsample/schema_packages/__init__.py
@@ -15,3 +15,18 @@ schema_package_entry_point = NewSchemaPackageEntryPoint(
     name='NewSchemaPackage',
     description='New schema package entry point configuration.',
 )
+
+
+class CRC1414SchemaEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from crc1415testsample.schema_packages.CRC1415_A04_schema import m_package
+
+        return m_package
+
+
+CRC1415_A04_schema = CRC1414SchemaEntryPoint(
+    name='CRC1415Schema',
+    description='New test schema package for CRC1415.',
+)
