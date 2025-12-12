b49c4b3e3b156497267aedff2793c82f83f4ebae
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2025-12-12 15:47:55 +0100

    * corrected test case for SEM

--- tests/schema_packages/test_crc1415plugin.py
+++ tests/schema_packages/test_crc1415plugin.py
@@ -10,7 +10,7 @@ def test_schema_package():
     print("Run Sample")
 
     #assert entry_archive.data.data_file == 'test.xyd' 
-    assert entry_archive.data.data_as_tif_or_tiff_file == ['testSEM.tif', 'testSEM3.tiff'] #'testSEM.tif' 
+    assert entry_archive.data.data_as_tif_or_tiff_file == ['test_file_SEM_01.tif', 'test_file_SEM_03.tiff'] #'testSEM.tif' 
     #assert entry_archive.data.data_file == 'test.csv' 
     #assert entry_archive.data.data_file == 'testSEM.jpg' 
     
