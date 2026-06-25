046de97945832a3d335c74606efa7ab74e8ea7a7
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2026-06-25 14:04:41 +0200

    added imports for __init.py__ for preloading schemas

--- src/crc1415plugin/schema_packages/schemas_measurement/__init__.py
+++ src/crc1415plugin/schema_packages/schemas_measurement/__init__.py
@@ -1,9 +1,9 @@
-# from .MeasurementAdsorption import MeasurementAdsorption
-# from .MeasurementCV import MeasurementCV
-# from .MeasurementGeneric import MeasurementGeneric
-# from .MeasurementIR import MeasurementIR
-# from .MeasurementRaman import MeasurementRaman
-# from .MeasurementSEM import MeasurementSEM
-# from .MeasurementTEM import MeasurementTEM
-# from .MeasurementTGA import MeasurementTGA
-# from .MeasurementXRD import MeasurementXRD
+from .MeasurementAdsorption import MeasurementAdsorption
+from .MeasurementCV import MeasurementCV
+from .MeasurementGeneric import MeasurementGeneric
+from .MeasurementIR import MeasurementIR
+from .MeasurementRaman import MeasurementRaman
+from .MeasurementSEM import MeasurementSEM
+from .MeasurementTEM import MeasurementTEM
+from .MeasurementTGA import MeasurementTGA
+from .MeasurementXRD import MeasurementXRD
